d5dfca727b3084d24b825f9ee910f90ed695ecd0
Ximfo <Ximffo@gmail.com>
2023-11-29 12:32:30 +0100

    add list

--- src/list/List.js
+++ src/list/List.js
@@ -1,7 +1,59 @@
 import React from "react";
 
+const API_URL = 'http://localhost:3000/anidb';
+
+const fetchdatabase = await fetch(`${API_URL}`)
+    .then(response=>response.json())
+    .then(database => {return database})
+    .catch(error=>{console.log(error)});
+
+const Animedatabase=fetchdatabase.sort((a,b)=>
+    a.title.toUpperCase().localeCompare(b.title.toUpperCase()))
+
+console.log(Animedatabase)
 export const List =()=> {
     return(
-        <h1>tutaj lista</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th className={'aniTitle'}>TITLE</th>
+                <th>GENRE</th>
+                <th>EPISODES</th>
+                <th>DURATION</th>
+                <th>TYPE</th>
+                <th>STUDIO</th>
+                <th>YEAR</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>{
+                    Animedatabase.map((element, index) => {return (<li key={element.mal_id} >{index}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} className={'aniTitle'}>{element.title}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} >{element.genre}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} >{element.episodes}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} >{element.duration}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} >{element.type}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} >{element.studio}</li>)})
+                }</td>
+                <td>{
+                    Animedatabase.map(element=>{return (<li key={element.mal_id} >{element.year}</li>)})
+                }</td>
+            </tr>
+            </tbody>
+        </table>
     )
 }
